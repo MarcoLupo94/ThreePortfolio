3eb34d99d6494efab9ddf158bd77335b065e840d
marco <marco.lupo@missionlabs.co.uk>
2023-03-22 20:31:19 +0000

    Added particles

--- src/components/molecules/Background.jsx
+++ src/components/molecules/Background.jsx
@@ -1,12 +1,16 @@
-import { Environment, Stars } from '@react-three/drei'
-
+import { Environment } from '@react-three/drei'
+import { useRef } from 'react'
+import Particles from '../atoms/Particles'
 export const Background = () => {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+  const mouse = useRef([0, 0])
+
   return (
     <>
+      <pointLight distance={100} intensity={4} color='white' />
+
       <Environment preset='night' />
-      <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={0.5} />
-      <Stars radius={40} depth={60} count={5000} factor={4} saturation={0} fade speed={0.5} />
-      <Stars radius={30} depth={20} count={5000} factor={4} saturation={0} fade speed={0.5} />
+      <Particles count={isMobile ? 5000 : 10000} mouse={mouse} />
       <color args={['#090D17']} attach='background' />
     </>
   )
